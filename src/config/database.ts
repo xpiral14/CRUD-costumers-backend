4d967b74eb89398dde1823d8fdd899457573f328
Samuel Reis <sa.reis1999@gmail.com>
2020-09-06 15:24:28 -0300

    Adicionado configuração de banco de dados

---
+++ src/config/database.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import path from 'path';
+
+console.log("passei aqui")
+const databaseConfig = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  logging: process.env.NODE_ENV === 'development' ? console.log : false,
+  dialectOptions: {
+    useUTC: false, //for reading from database
+    timezone: 'Etc/GMT0',
+  },
+  dialect: 'mariadb',
+  define: {
+    timestamps: true,
+    underscored: false,
+    underscoredAll: false,
+  },
+  models: [path.resolve('src', 'models')],
+};
+
+export default databaseConfig;
